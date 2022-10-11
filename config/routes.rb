ce4aa0bb41831b51491f67e7df8b8badd8e5a132
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-10-11 20:34:17 +0300

    added models and migratins

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   
-  # Routing logic: fallback requests for React Router.
-  # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # resources :admins
+  # resources :buyers
+  # # Routing logic: fallback requests for React Router.
+  # # Leave this here to help deploy your app later!
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  post "/signup_buyer", to: "buyers#create"
+  get "/me_buyer", to: "buyers#show"
+  post "/login_buyer", to: "sessions#create"
+  delete "/logout_buyer", to: "sessions#destroy"
+
+  post "/signup_admin", to: "buyers#create"
+  get "/me_admin", to: "buyers#show"
+  post "/login_admin", to: "sessions#create"
+  delete "/logout_admin", to: "sessions#destroy"
 end
