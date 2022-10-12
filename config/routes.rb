98a52712ba930fad37961e7fe22fb099b2fad37d
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-10-12 14:05:33 -0700

    changes on wishlist

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   post "/login_admin", to: "sessions#create"
   delete "/logout_admin", to: "sessions#destroy"
 
-  resources :users
+  resources :wishList
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
