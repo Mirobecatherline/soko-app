ea3fe8f89711ab1deca60a6a4cf58d13ea347b4f
Bonyonte <magogobonyonte@gmail.com>
2022-10-12 13:12:14 +0300

    create product instances and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,38 @@
 class ProductsController < ApplicationController
+    def index
+        products = Product.all
+        render json: products
+    end
+
+    def show 
+        product= Product.find[params[:id]]
+        render json: product 
+    end
+
+    def create 
+        product = Product.create(params.permit(:product_name))
+        render json: product, status: :created
+    end
+
+    def update 
+        product = Product.find_by(id: params[:id])
+        if product 
+            product.update(product_params)
+            render json: product 
+        else 
+            render json: {error: "Product was not available"}
+        end
+    end
+
+    def destroy
+        product = Product.find(params[:id])
+        product.destroy
+        head :no_content 
+    end
+
+    private 
+    
+    def product_params
+        params.permit(:product_name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
   post "/login_admin", to: "sessions#create"
   delete "/logout_admin", to: "sessions#destroy"
 
+  resources :products, only: [:index, :show, :update, :create, :destroy]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
