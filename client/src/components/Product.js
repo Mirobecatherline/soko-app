9ab74bf0fccad22629c9af1525fbf0f930886715
Bonyonte <magogobonyonte@gmail.com>
2022-10-13 11:17:04 +0300

    fetch products data

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,44 +1,51 @@
-import React, { useState } from 'react'
-import Card from './Card'
+import React, { useState, useEffect } from 'react';
+import Card from './Card';
+import '../App';
 
 function Product() {
 
         const [products, setProducts] = useState([]);
-        const [ search, setSearch] = useState(" ");
-    
-        useEffect(() => {
-            fetch('http://127.0.0.1:3000/products')
-            .then(r =>r.json())
-            .then((data) => setProducts(data))
-            
-        },[])
-    
-        const searchProduct = (e) =>{
-          if(e.key==="Enter"){
-            axios.get("http://127.0.0.1:3000/products")
-            .then(res =>setProducts(res.data))
-          }
+        // const [ search, setSearch] = useState(" ");
+
+        const fetchData = () =>{
+            fetch('/products')
+            .then(r =>{
+              return r.json()
+            })
+            .then((data) => {
+              setProducts(data)
+            })
         }
+            useEffect(() => {
+              fetchData()
+            }, [])
+
+        // const searchProduct = (e) =>{
+        //   if(e.key==="Enter"){
+        //     axios.get("/products")
+        //     .then(res =>setProducts(res.data))
+        //   }
+        // }
     
-        const handleOnSearch =(e) => {
-          setSearch(e.target.value)
-        }
+        // const handleOnSearch =(e) => {
+        //   setSearch(e.target.value)
+        // }
     
-          const filterProducts = products.filter((product) => product.productName.includes(search))
+          //const filterProducts = products.filter((product) => product.productName.includes(search))
     
     
-          const productNames = products.map((product) => product.productName)
+           //const productNames = products.map((product) => product.productName)
       return (
         <>
-        <div className="row2">
-                <h2>Find a product</h2>
+        {/* <div className="row2">
+                {/* <h2>Find a product</h2>
                 <div className="search">
                     <input type="text" placeholder="Enter Your Product Name" 
                    value={search} onChange={handleOnSearch}
                    onKeyPress={searchProduct}
                    />
-                </div>
-            </div>
+                </div> }
+            </div >
         <div className="product-list">{filterProducts.map((product) =>{
           return <Card name={product.productName} 
           image={product.productImageUrl} 
@@ -47,7 +54,14 @@ function Product() {
           description={product.productDescription} 
           key={product.id}/>
         })}
-        </div>
+        </div>  */}
+        {products.length > 0 && (
+          <ul>
+            {products.map(product => (
+              <li key={product.id}>{product.productName}</li>
+            ))}
+          </ul>
+        )}
         </>
       )
 }
